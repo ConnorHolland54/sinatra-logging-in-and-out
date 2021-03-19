a11d3ada0896f161361e7e93ee9ab211d72f65d4
Connor Holland <supersoccergoalie123@gmail.com>
2021-03-18 23:01:41 -0600

    commit tree 6abf040820ff1d95d126c61a32b6a1a6267018b8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 require_relative '../../config/environment'
 class ApplicationController < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
   configure do
     set :views, Proc.new { File.join(root, "../views/") }
     enable :sessions unless test?
@@ -11,11 +12,20 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params[:username])
+    if @user
+      session[:user_id] = @user.id
+      redirect "/account"
+    end
   end
 
   get '/account' do
-
+    @user = User.find(session[:user_id])
+    if @user
+      erb :account
+    else 
+      erb :error
+    end
   end
 
   get '/logout' do
